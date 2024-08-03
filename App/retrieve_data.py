9f73d602aac7785cb2be464db7b3611c6e5572e0
Giovanni Solano Porras <43626029+porrasgp@users.noreply.github.com>
2024-08-03 00:16:11 -0600

    Last Update

--- App/retrieve_data.py
+++ App/retrieve_data.py
@@ -18,7 +18,7 @@ request = {
 client = cdsapi.Client()
 
 # Define the folder where the data will be saved
-folder_name = 'Data'
+folder_name = './Data'
 if not os.path.exists(folder_name):
     os.makedirs(folder_name)
 
