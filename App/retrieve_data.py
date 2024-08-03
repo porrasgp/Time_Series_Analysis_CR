2296b85e89a65cac3a92459886a1ebd072e3ad1d
Giovanni Solano Porras <43626029+porrasgp@users.noreply.github.com>
2024-08-03 17:18:06 -0600

    Update retrieve_data.py

--- App/retrieve_data.py
+++ App/retrieve_data.py
@@ -1,3 +1,4 @@
+import io
 import os
 import cdsapi
 import boto3
@@ -21,24 +22,23 @@ request = {
     'month': ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
     'day': ['10', '20', '28', '30', '31'],
     'growing_season': ['1st_season_per_campaign'],
-    'harvest_year': '2023'
+    'harvest_year': '2023',
+    'data_format': 'grib'
 }
 
 client = cdsapi.Client()
-folder_name = 'App/Data'
-zip_file_path = os.path.join(folder_name, 'data.zip')
 
-# Ensure the directory exists
-os.makedirs(folder_name, exist_ok=True)
+# In-memory buffer
+buffer = io.BytesIO()
 
-print(f"Attempting to save data to: {zip_file_path}")
+print("Attempting to retrieve and upload data directly to S3...")
 
 try:
-    # Retrieve and download data
-    client.retrieve(dataset, request).download(zip_file_path)
-    print(f"Data download completed. File saved to: {zip_file_path}")
-    
-    # Upload the file to S3
+    # Retrieve data and save it to the buffer
+    client.retrieve(dataset, request).download(target=None, callback=lambda data: buffer.write(data))
+    buffer.seek(0)  # Go back to the start of the buffer
+
+    # Upload the buffer content to S3
     s3_client = boto3.client(
         's3',
         aws_access_key_id=AWS_ACCESS_KEY_ID,
@@ -46,9 +46,9 @@ try:
         region_name=AWS_REGION
     )
 
-    s3_key = 'App/Data/data.zip'
-    s3_client.upload_file(zip_file_path, BUCKET_NAME, s3_key)
+    s3_key = 'App/Data/data.grib'
+    s3_client.upload_fileobj(buffer, BUCKET_NAME, s3_key)
     print(f"File uploaded to S3 bucket {BUCKET_NAME} with key {s3_key}")
 
 except Exception as e:
-    print(f"Error: {e}") 
+    print(f"Error: {e}")
