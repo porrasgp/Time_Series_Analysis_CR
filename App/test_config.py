a10c57eb382043d291024d0bf7dd10e0b1b0d627
Giovanni Solano Porras <43626029+porrasgp@users.noreply.github.com>
2024-08-03 21:35:22 -0600

    Delete App/test_config.py

--- App/test_config.py
+++
@@ -1,8 +0,0 @@
-import cdsapi
-
-# Initialize the CDS API client
-try:
-    client = cdsapi.Client()
-    print("CDS API client initialized successfully.")
-except Exception as e:
-    print(f"Error initializing CDS API client: {e}")
