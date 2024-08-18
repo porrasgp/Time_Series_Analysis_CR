84eee62349a9e82b6101d34785daea4d1180b07e
Giovanni Solano Porras <43626029+porrasgp@users.noreply.github.com>
2024-08-18 13:00:50 -0600

    Update S3ConnectionML.py 2019

--- App/S3ConnectionML.py
+++ App/S3ConnectionML.py
@@ -125,7 +125,7 @@ def process_year_data(year, variables):
 
 # Variables y años
 variables = ["crop_development_stage", "total_above_ground_production", "total_weight_storage_organs"]
-years = ["2019", "2020", "2021", "2022", "2023"]
+years = ["2019"]
 
 # Crear y ejecutar hilos para procesar los datos de cada año
 threads = []
